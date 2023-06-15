426c0641466378c11e0c7aab4a082edcfe966204
Jelmer Draaijer <info@jelmert.nl>
2023-06-15 10:02:43 +0200

    Unify client init method & reorder methods

--- src/kikinzage/client/asyncio.py
+++ src/kikinzage/client/asyncio.py
@@ -19,38 +19,30 @@ class AsyncClient(KikinzageBaseClient):
 
     client: httpx.AsyncClient
 
-    def __init__(
+    async def __aenter__(self) -> "AsyncClient":
+        await self.client.__aenter__()
+        return self
+
+    async def __aexit__(
         self,
-        username: str,
-        password: str,
-        base_url: str = "https://service10.kadaster.nl/kik-inzage-eto/v6/",
-        formaat: Formaat = Formaat.JSON,
-        klantreferentie: Optional[str] = None,
-        gebruikeridentificatie: Optional[str] = None,
-        hyperlinkopproduct: Optional[bool] = None,
-        inkoopnummer: Optional[str] = None,
-        referentienummer: Optional[str] = None,
-        **httpx_kwargs: Any,
+        exc_type: Optional[Type[BaseException]] = None,
+        exc_value: Optional[BaseException] = None,
+        traceback: Optional[TracebackType] = None,
     ) -> None:
-        super().__init__(
-            username=username,
-            password=password,
-            base_url=base_url,
-            formaat=formaat,
-            klantreferentie=klantreferentie,
-            gebruikeridentificatie=gebruikeridentificatie,
-            hyperlinkopproduct=hyperlinkopproduct,
-            inkoopnummer=inkoopnummer,
-            referentienummer=referentienummer,
-        )
-
-        httpx_kwargs.setdefault("base_url", base_url)
-        httpx_kwargs.setdefault("auth", (self.username, self.password))
-
-        self.client = self.create_client(**httpx_kwargs)
+        await self.client.__aexit__(exc_type, exc_value, traceback)
 
     def create_client(self, **httpx_kwargs: Any) -> httpx.AsyncClient:
         return httpx.AsyncClient(**httpx_kwargs)
+
+    async def send(
+        self,
+        request: httpx.Request,
+        model: Type[ResponseType],
+        status_code_success: int = 200,
+    ) -> ResponseType:
+        response = await self.client.send(request)
+
+        return self.process_response(response, model, status_code_success)
 
     async def eigendomsinformatie_kadastraalobjectidentificatie(
         self,
@@ -75,25 +67,3 @@ class AsyncClient(KikinzageBaseClient):
         )
 
         return await self.send(request, models.Eigendomsinformatie)
-
-    async def send(
-        self,
-        request: httpx.Request,
-        model: Type[ResponseType],
-        status_code_success: int = 200,
-    ) -> ResponseType:
-        response = await self.client.send(request)
-
-        return self.process_response(response, model, status_code_success)
-
-    async def __aenter__(self) -> "AsyncClient":
-        await self.client.__aenter__()
-        return self
-
-    async def __aexit__(
-        self,
-        exc_type: Optional[Type[BaseException]] = None,
-        exc_value: Optional[BaseException] = None,
-        traceback: Optional[TracebackType] = None,
-    ) -> None:
-        await self.client.__aexit__(exc_type, exc_value, traceback)
--- src/kikinzage/client/default.py
+++ src/kikinzage/client/default.py
@@ -18,38 +18,18 @@ class DefaultClient(KikinzageBaseClient):
 
     client: httpx.Client
 
-    def __init__(
-        self,
-        username: str,
-        password: str,
-        base_url: str = "https://service10.kadaster.nl/kik-inzage-eto/v6/",
-        formaat: Formaat = Formaat.JSON,
-        klantreferentie: Optional[str] = None,
-        gebruikeridentificatie: Optional[str] = None,
-        hyperlinkopproduct: Optional[bool] = None,
-        inkoopnummer: Optional[str] = None,
-        referentienummer: Optional[str] = None,
-        **httpx_kwargs: Any,
-    ) -> None:
-        super().__init__(
-            username=username,
-            password=password,
-            base_url=base_url,
-            formaat=formaat,
-            klantreferentie=klantreferentie,
-            gebruikeridentificatie=gebruikeridentificatie,
-            hyperlinkopproduct=hyperlinkopproduct,
-            inkoopnummer=inkoopnummer,
-            referentienummer=referentienummer,
-        )
-
-        httpx_kwargs.setdefault("base_url", base_url)
-        httpx_kwargs.setdefault("auth", (self.username, self.password))
-
-        self.client = self.create_client(**httpx_kwargs)
-
     def create_client(self, **httpx_kwargs: Any) -> httpx.Client:
         return httpx.Client(**httpx_kwargs)
+
+    def send(
+        self,
+        request: httpx.Request,
+        model: Type[ResponseType],
+        status_code_success: int = 200,
+    ) -> ResponseType:
+        response = self.client.send(request)
+
+        return self.process_response(response, model, status_code_success)
 
     def eigendomsinformatie_kadastraalobjectidentificatie(
         self,
@@ -160,13 +140,3 @@ class DefaultClient(KikinzageBaseClient):
         )
 
         return self.send(request, models.Eigendomsinformatie)
-
-    def send(
-        self,
-        request: httpx.Request,
-        model: Type[ResponseType],
-        status_code_success: int = 200,
-    ) -> ResponseType:
-        response = self.client.send(request)
-
-        return self.process_response(response, model, status_code_success)
